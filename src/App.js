f02712f5d4cf925164b3d5b88a74cf1584a1f30d
aleswebgit <aleswebgit@gmail.com>
2022-01-09 21:12:41 +0100

    creados componentes todo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
 // import './App.css';
 
 const todos = [
@@ -10,14 +15,14 @@ const todos = [
 function App(props) {
   return (
     <React.Fragment>
-      {/* <TodoCounter /> */}
-      {/* <TodoSearch /> */}
-      {/* <TodoList> */}
-        {/* {todos.map(todo => ( */}
-          {/* <TodoItem key={todo.text} todo={todo} /> */}
-        {/* ))} */}
-      {/* </TodoList> */}
-      {/* <CreateTodoButton /> */}
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text}/>
+        ))}
+      </TodoList>
+      <CreateTodoButton />
     </React.Fragment>
   );
 }
