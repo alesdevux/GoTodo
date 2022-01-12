40a97f659ab46b4f6cac854c0593e80d8c8a89cd
aleswebgit <aleswebgit@gmail.com>
2022-01-12 15:45:55 +0100

    icons components, con iconos SVG

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { TodoContext } from "../TodoContext";
 import './CreateTodoButton.css';
+import {TodoIcon} from "../TodoIcon";
 
 function CreateTodoButton() {
   const {openModal, setOpenModal} = React.useContext(TodoContext);
@@ -14,7 +15,10 @@ function CreateTodoButton() {
       className="createTodoButton"
       onClick={onClickButton}
     >
-      {openModal ? 'x' : '+'}
+      {openModal ? 
+        <TodoIcon type="close" /> :
+        <TodoIcon type="plus" />
+      }
     </button>
   );
 }
---
+++ src/TodoIcon/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import './TodoIcon.css';
+import {ReactComponent as CheckSVG} from '../Icons/check.svg';
+import {ReactComponent as DeleteSVG} from '../Icons/delete.svg';
+import {ReactComponent as PlusSVG} from '../Icons/plus.svg';
+import {ReactComponent as CloseSVG} from '../Icons/close.svg';
+
+function TodoIcon({type, color, onClick}) {
+  const iconTypes = {
+    "check": color => (
+      <CheckSVG
+        className="Icon-svg Icon-svg--check"
+        fill={color}
+      />
+    ),
+    "delete": color => (
+      <DeleteSVG
+        className="Icon-svg Icon-svg--delete"
+        fill={color}
+      />
+    ),
+    "plus": color => (
+      <PlusSVG
+        className="Icon-svg Icon-svg--plus"
+        fill={color}
+      />
+    ),
+    "close": color => (
+      <CloseSVG
+        className="Icon-svg Icon-svg--close"
+        fill={color}
+      />
+    ),
+  };
+
+  return (
+    <span
+      className={`Icon-container Icon-container--${type}`}
+      onClick={onClick}
+    >
+      {iconTypes[type](color)}
+    </span>
+  );
+}
+
+export { TodoIcon };
